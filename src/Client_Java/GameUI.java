c2fd82e3ecd6ddc757e9249a565d7f6824c042c8
Jermin Odcheo <jerminsux6@gmail.com>
2023-05-18 23:48:01 +0800

    Fixed GameUI

---
+++ src/Client_Java/GameUI.java
@@ -0,0 +1,248 @@
+package Client_Java;
+
+import Server_Java.WordyServer;
+import Server_Java.corba.WordyCallback;
+
+import javax.swing.*;
+
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+import static Client_Java.Client.wordyCallback;
+import static Client_Java.Client.wordyImpl;
+
+public class GameUI extends javax.swing.JFrame {
+    static String username;
+    public GameUI(String username) {
+        this.username = username;
+        initComponents();
+        System.out.println(wordyImpl.playerInGameList());
+        playerListField.append(wordyImpl.playerInGameList().replace(",", "\n").substring(1, wordyImpl.playerInGameList().length() - 1));
+        jTextArea1.append(wordyImpl.getGeneratedLetter());
+        System.out.println(wordyImpl.getGeneratedLetter());
+
+
+    }
+
+    public void addListeners()
+    {
+        // Add your listeners here as usual
+        this.addWindowListener(new WindowListener()
+        {
+            @Override
+            public void windowOpened(WindowEvent e)
+            {
+
+                /* ... */
+            }
+
+            @Override
+            public void windowClosing(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowClosed(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowIconified(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowDeiconified(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowActivated(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowDeactivated(WindowEvent e) {
+
+            }
+
+            /* Other methods of WindowListener ... */
+        });
+    }
+    private void initComponents() {
+
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        playerListField = new javax.swing.JTextArea();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        wordBoxField = new javax.swing.JTextArea();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        jLabel1 = new javax.swing.JLabel();
+        inputField = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        sendButton = new javax.swing.JButton();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        timerField = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        getContentPane().setLayout(null);
+
+        playerListField.setBackground(new java.awt.Color(212, 250, 252));
+        playerListField.setColumns(20);
+        playerListField.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        playerListField.setForeground(new java.awt.Color(0, 102, 204));
+        playerListField.setRows(5);
+        playerListField.setBorder(javax.swing.BorderFactory.createMatteBorder(2, 2, 2, 2, new java.awt.Color(0, 153, 255)));
+        jScrollPane1.setViewportView(playerListField);
+
+        getContentPane().add(jScrollPane1);
+        jScrollPane1.setBounds(790, 130, 266, 300);
+
+        wordBoxField.setBackground(new java.awt.Color(227, 223, 253));
+        wordBoxField.setColumns(20);
+        wordBoxField.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        wordBoxField.setForeground(new java.awt.Color(204, 0, 204));
+        wordBoxField.setRows(5);
+        wordBoxField.setBorder(javax.swing.BorderFactory.createMatteBorder(2, 2, 2, 2, new java.awt.Color(255, 102, 255)));
+        jScrollPane2.setViewportView(wordBoxField);
+
+        getContentPane().add(jScrollPane2);
+        jScrollPane2.setBounds(22, 130, 716, 310);
+
+        jTextArea1.setBackground(new java.awt.Color(255, 204, 255));
+        jTextArea1.setColumns(20);
+        jTextArea1.setFont(new java.awt.Font("sansserif", 1, 18)); // NOI18N
+        jTextArea1.setForeground(new java.awt.Color(255, 51, 204));
+        jTextArea1.setLineWrap(true);
+        jTextArea1.setRows(5);
+        jTextArea1.setBorder(javax.swing.BorderFactory.createMatteBorder(3, 3, 3, 3, new java.awt.Color(255, 51, 255)));
+        jScrollPane3.setViewportView(jTextArea1);
+
+        getContentPane().add(jScrollPane3);
+        jScrollPane3.setBounds(790, 480, 270, 240);
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setText("WordyGame Beta v3.32.12");
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(22, 15, 490, 48);
+
+        inputField.setBackground(new java.awt.Color(204, 204, 255));
+        inputField.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        inputField.setForeground(new java.awt.Color(204, 51, 255));
+        inputField.setBorder(javax.swing.BorderFactory.createMatteBorder(2, 2, 2, 2, new java.awt.Color(255, 204, 153)));
+        inputField.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                inputFieldActionPerformed(evt);
+            }
+        });
+        getContentPane().add(inputField);
+        inputField.setBounds(30, 510, 530, 50);
+
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setText("Word Box");
+        getContentPane().add(jLabel2);
+        jLabel2.setBounds(20, 100, 117, 25);
+
+        sendButton.setBackground(new java.awt.Color(255, 51, 255));
+        sendButton.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        sendButton.setForeground(new java.awt.Color(255, 255, 255));
+        sendButton.setText("Enter");
+        sendButton.setBorderPainted(false);
+        sendButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                sendButtonActionPerformed(evt);
+            }
+        });
+        getContentPane().add(sendButton);
+        sendButton.setBounds(220, 590, 160, 50);
+
+        jLabel4.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel4.setText("Your Letters");
+        getContentPane().add(jLabel4);
+        jLabel4.setBounds(790, 450, 117, 25);
+
+        jLabel5.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel5.setText("Player List");
+        getContentPane().add(jLabel5);
+        jLabel5.setBounds(790, 100, 117, 25);
+
+        jLabel6.setIcon(new javax.swing.ImageIcon("src/Server_Java/WORD.png")); // NOI18N
+        getContentPane().add(jLabel6);
+        jLabel6.setBounds(400, 440, 410, 310);
+
+        timerField.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
+        timerField.setForeground(new java.awt.Color(255, 255, 255));
+        getContentPane().add(timerField);
+        timerField.setBounds(1000, 30, 70, 50);
+
+        jLabel3.setIcon(new javax.swing.ImageIcon("src/Client_Java/WORD (5).png")); // NOI18N
+        getContentPane().add(jLabel3);
+        jLabel3.setBounds(710, -20, 380, 150);
+
+        jLabel7.setIcon(new javax.swing.ImageIcon("src/Client_Java/17250835.png")); // NOI18N
+        getContentPane().add(jLabel7);
+        jLabel7.setBounds(0, 0, 1110, 770);
+
+        setBounds(0, 0, 1125, 776);
+    }
+
+    private void inputFieldActionPerformed(java.awt.event.ActionEvent evt) {
+        // TODO add your handling code here:
+    }
+
+    private void sendButtonActionPerformed(java.awt.event.ActionEvent evt) {
+        try {
+        wordyImpl.playWord(username,inputField.getText());}
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void startGameUI(String username) {
+
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException |
+                 UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(GameUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+
+                new GameUI(username).setVisible(true);
+            }
+        });
+    }
+
+    private static javax.swing.JTextField inputField;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private static javax.swing.JTextArea jTextArea1;
+    private javax.swing.JTextArea playerListField;
+    private javax.swing.JButton sendButton;
+    private javax.swing.JLabel timerField;
+    private javax.swing.JTextArea wordBoxField;
+}
+
---
+++ src/Server_Java/WordyServer.java
@@ -0,0 +1,268 @@
+package Server_Java;
+
+import Client_Java.myConnection;
+import Server_Java.corba.*;
+import org.omg.CORBA.ORB;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.*;
+
+
+public class WordyServer extends wordyPOA {
+    private static final List<String> players = new ArrayList<>();
+    private final List<String> rounds = new ArrayList<>();
+    private static final List<String> lobbyPlayers = new ArrayList<>();
+    private final ArrayList<String> playersInGame = new ArrayList<>();
+    private int countdown = 10;
+    private boolean isGameStarted;
+    private String letters;
+
+
+    public String login(String username, String password) {
+        if (players.contains(username)){
+            System.out.println("Already Logged In!");
+            return "LoggedIn";
+        }
+        try {
+            boolean check = verifyUsername(username);
+            if (!check) {
+                System.out.println(username + " : " + "Account not found");
+                return "NotFound";
+            } else {
+                PreparedStatement preparedStatement;
+                ResultSet resultSet;
+                preparedStatement = myConnection.getConnection().prepareStatement("SELECT * FROM users WHERE user_username = ? AND user_password = ?");
+                preparedStatement.setString(1, username);
+                preparedStatement.setString(2, password);
+                resultSet = preparedStatement.executeQuery();
+                if (resultSet.next()) {
+                        players.add(username);
+                        System.out.println(username + " : " + "Successfully Logged In");
+                        return "Found";
+                } else {
+                    return "Invalid";
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return "Error";
+        }
+
+    }
+    @Override
+    public void exit(String username) {
+        players.remove(username);
+        playersInGame.remove(username);
+        System.out.println(username + ": Logged Out!");
+    }
+    public boolean status(String playerName) {
+        return playersInGame.contains(playerName);
+    }
+    public boolean verifyUsername(String username) {
+        ResultSet resultSet;
+        try {
+            PreparedStatement preparedStatement;
+            preparedStatement = myConnection.getConnection().prepareStatement("SELECT * FROM `users` WHERE user_username =?");
+            preparedStatement.setString(1, username);
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                String user = resultSet.getString("user_username");
+                if (username.equals(user)) {
+                    return true;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+    //Player join the lobby
+    private boolean countdownStarted;
+    public boolean joinLobby(String playerName) {
+        if (lobbyPlayers.size() >= 5) {
+            System.out.println("Lobby is Full");
+            return false;
+        }
+        lobbyPlayers.add(playerName);
+        System.out.println("Player " + playerName + " joined the lobby.");
+        if (lobbyPlayers.size() >=2 && !countdownStarted) {
+            timerServer();
+            countdownStarted = true;
+        }
+        return true;
+    }
+    private Timer timer;
+    //METHOD TIMER TO START THE LOBBY
+    public void timerServer(){
+        new Thread(() -> {
+            while (countdown > 0) {
+                System.out.println("Countdown: " + countdown);
+                countdown--;
+                try {
+                    Thread.sleep(1000); // Sleep for 1 second
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (lobbyPlayers.size() >= 2) {
+                startGame();
+            } else {
+                countdownStarted = false;
+            }
+
+        }).start();
+    }
+    public double gettimer() {
+        return countdown;
+    }
+    public String getGeneratedLetter(){
+        return letters;
+    }
+
+    @Override
+    public String playerInGameList() {
+        return lobbyPlayers.toString();
+    }
+
+    @Override
+    public void generateLetters() {
+
+    }
+
+    private Server_Java.WordyServer.WordyCallback[] clients;
+
+
+    private void startGame() {
+        if (lobbyPlayers.size() < 2) {
+            System.out.println("Cannot start game. Not enough players.");
+            lobbyPlayers.clear();
+            countdown = 10;
+            return;
+        }
+        System.out.println("Starting game with players: " + lobbyPlayers.toString());
+        //Generate 17 random letters with 5-7 vowels
+        generateRandomLetters();
+        System.out.println("GAME STARTING WITH: " + letters);
+
+        countdown = 10;
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        lobbyPlayers.clear();
+    }
+
+    // Play Word method
+    public void playWord(String playerName, String word) throws GameException {
+        // check if word can be formed from letters
+        for (char c : word.toCharArray()) {
+            if (letters.indexOf(c) == -1) {
+                throw new GameException("Word cannot be formed from letters.");
+            }
+        }
+        System.out.println(playerName + " played the word: " + word);
+    }
+
+    private boolean isWordValidForRound(String word) {
+        if (rounds.size() == 0) {
+            return true;
+        }
+        String lastWord = rounds.get(rounds.size()-1);
+        char lastChar = lastWord.charAt(lastWord.length() - 1);
+        char firstChar = word.charAt(0);
+        if (lastChar == firstChar) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void startNewRound() {
+        letters = generateRandomLetters();
+        rounds.add(letters);
+        System.out.println("Starting new round with letters: " + letters);
+    }
+
+    private String generateRandomLetters() {
+        List<Character> letter = new ArrayList<Character>();
+        Random random = new Random();
+        int numVowels = random.nextInt(3) + 5; // 5 to 7 vowels
+        for (int i = 0; i < numVowels; i++) {
+            letter.add(getRandomVowel());
+        }
+        for (int i = numVowels; i < 17; i++) {
+            letter.add(getRandomConsonant());
+        }
+        Collections.shuffle(letter);
+        StringBuilder sb = new StringBuilder();
+        for (char c : letter) {
+            sb.append(c);
+        }
+
+        return  letters = sb.toString();
+    }
+    private char getRandomVowel() {
+        String vowels = "AEIOU";
+        return vowels.charAt(new Random().nextInt(vowels.length()));
+    }
+
+    private char getRandomConsonant() {
+        String consonants = "BCDFGHJKLMNPQRSTVWXYZ";
+        return consonants.charAt(new Random().nextInt(consonants.length()));
+    }
+    private Set<String> words;
+    // implementation to check if word is valid using .txt file
+    private boolean isValidWord(String word) {
+        try {
+            words = new HashSet<>();
+            BufferedReader reader = new BufferedReader(new FileReader("src/Server_Java/words.txt"));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                words.add(line.trim().toUpperCase());
+            }
+            reader.close();
+        } catch (IOException e) {
+            System.err.println("Failed to load dictionary: " + e.getMessage());
+        }
+
+        return words.contains(word.toUpperCase());
+    }
+
+    public void leaveGame(String playerName) throws GameException {
+        if (playersInGame.contains(playerName) || lobbyPlayers.contains(playerName)) {
+            playersInGame.remove(playerName);
+            lobbyPlayers.remove(playerName);
+            if (playersInGame.size() == 0 || lobbyPlayers.size() == 0) {
+                isGameStarted = false;
+            }
+        } else {
+            throw new GameException("Player " + playerName + " is not in the game");
+        };
+    }
+
+
+    @Override
+    public double lobbyPlayerCount() {
+        return lobbyPlayers.size();
+    }
+
+        public static class WordyCallback extends WordyCallbackPOA {
+            private final List<WordyCallback> callbacks = new ArrayList<>();
+            private ORB orb;
+
+            public void setORB(ORB orb_val) {
+                orb = orb_val;
+            }
+
+            @Override
+            public void sendGeneratedLetter(String letters) {
+                System.out.println("Received generated letters: " + letters);
+            }
+        }
+}
