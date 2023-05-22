db172e3b229f732559dd135df73565d2e612d3e8
Anne Marie Factor <2224980@slu.edu.ph>
2023-05-22 23:28:16 +0800

    minor fixes on ClientUI

--- src/Client_Java/ClientUI.java
+++ src/Client_Java/ClientUI.java
@@ -3,7 +3,7 @@ package Client_Java;
 import static Client_Java.Client.wordyImpl;
 
 public class ClientUI extends javax.swing.JFrame {
-    private javax.swing.JLabel backgroundPane;
+    private javax.swing.JLabel backgroundImage;
     private javax.swing.JPanel backgroundPanel;
     private javax.swing.JButton exitButton;
     private javax.swing.JLayeredPane jLayeredPane2;
@@ -16,20 +16,20 @@ public class ClientUI extends javax.swing.JFrame {
         this.username = username;
         initComponents();
     }
-
-    @SuppressWarnings("unchecked")
     private void initComponents() {
 
         backgroundPanel = new javax.swing.JPanel();
         startGameButton = new javax.swing.JButton();
+        leaderboardButton = new javax.swing.JButton();
         welcomeMessage = new javax.swing.JLabel();
         logo = new javax.swing.JLabel();
         jLayeredPane2 = new javax.swing.JLayeredPane();
-        backgroundPane = new javax.swing.JLabel();
-        leaderboardButton = new javax.swing.JButton();
+        backgroundImage = new javax.swing.JLabel();
         exitButton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMinimumSize(new java.awt.Dimension(900, 550));
+        getContentPane().setLayout(null);
 
         backgroundPanel.setBackground(new java.awt.Color(33, 42, 62));
 
@@ -44,35 +44,6 @@ public class ClientUI extends javax.swing.JFrame {
             }
         });
 
-        welcomeMessage.setFont(new java.awt.Font("Segoe UI", 1, 39)); // NOI18N
-        welcomeMessage.setForeground(new java.awt.Color(255, 255, 255));
-        welcomeMessage.setText("WORD GAME BETA v 3.32.71");
-
-        logo.setIcon(new javax.swing.ImageIcon("src/Server_Java/WORD.png")); // NOI18N
-
-        jLayeredPane2.setBackground(new java.awt.Color(255, 255, 255));
-        jLayeredPane2.setForeground(new java.awt.Color(238, 241, 255));
-
-        backgroundPane.setIcon(new javax.swing.ImageIcon("src/Client_Java/17250835.png")); // NOI18N
-        backgroundPane.setText("");
-
-        jLayeredPane2.setLayer(backgroundPane, javax.swing.JLayeredPane.DEFAULT_LAYER);
-
-        javax.swing.GroupLayout jLayeredPane2Layout = new javax.swing.GroupLayout(jLayeredPane2);
-        jLayeredPane2.setLayout(jLayeredPane2Layout);
-        jLayeredPane2Layout.setHorizontalGroup(
-                jLayeredPane2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(jLayeredPane2Layout.createSequentialGroup()
-                                .addComponent(backgroundPane, javax.swing.GroupLayout.PREFERRED_SIZE, 904, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(0, 37, Short.MAX_VALUE))
-        );
-        jLayeredPane2Layout.setVerticalGroup(
-                jLayeredPane2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(jLayeredPane2Layout.createSequentialGroup()
-                                .addComponent(backgroundPane, javax.swing.GroupLayout.PREFERRED_SIZE, 524, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(0, 0, Short.MAX_VALUE))
-        );
-
         leaderboardButton.setFont(new java.awt.Font("Arial Rounded MT Bold", 1, 18)); // NOI18N
         leaderboardButton.setForeground(new java.awt.Color(238, 241, 255));
         leaderboardButton.setText("LEADERBOARD");
@@ -84,10 +55,36 @@ public class ClientUI extends javax.swing.JFrame {
             }
         });
 
+        welcomeMessage.setFont(new java.awt.Font("Segoe UI", 1, 39)); // NOI18N
+        welcomeMessage.setForeground(new java.awt.Color(255, 255, 255));
+        welcomeMessage.setText("WORD GAME BETA v 3.32.71");
+
+        logo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/clientui/finalclientui/logo.png"))); // NOI18N
+
+        backgroundImage.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/clientui/finalclientui/background.jpg"))); // NOI18N
+
+        jLayeredPane2.setLayer(backgroundImage, javax.swing.JLayeredPane.DEFAULT_LAYER);
+
+        javax.swing.GroupLayout jLayeredPane2Layout = new javax.swing.GroupLayout(jLayeredPane2);
+        jLayeredPane2.setLayout(jLayeredPane2Layout);
+        jLayeredPane2Layout.setHorizontalGroup(
+                jLayeredPane2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGap(0, 905, Short.MAX_VALUE)
+                        .addGroup(jLayeredPane2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(backgroundImage, javax.swing.GroupLayout.PREFERRED_SIZE, 905, Short.MAX_VALUE))
+        );
+        jLayeredPane2Layout.setVerticalGroup(
+                jLayeredPane2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGap(0, 524, Short.MAX_VALUE)
+                        .addGroup(jLayeredPane2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(backgroundImage, javax.swing.GroupLayout.PREFERRED_SIZE, 524, Short.MAX_VALUE))
+        );
+
         exitButton.setFont(new java.awt.Font("Arial Rounded MT Bold", 1, 18)); // NOI18N
-        exitButton.setForeground(new java.awt.Color(238, 241, 255));
+        exitButton.setForeground(new java.awt.Color(255, 255, 255));
         exitButton.setText("EXIT");
         exitButton.setBorder(null);
+        exitButton.setBorderPainted(false);
         exitButton.setContentAreaFilled(false);
         exitButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -109,56 +106,47 @@ public class ClientUI extends javax.swing.JFrame {
                                                 .addComponent(logo)
                                                 .addGroup(backgroundPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                                         .addGroup(backgroundPanelLayout.createSequentialGroup()
-                                                                .addGap(135, 135, 135)
-                                                                .addComponent(leaderboardButton, javax.swing.GroupLayout.PREFERRED_SIZE, 151, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                                        .addGroup(backgroundPanelLayout.createSequentialGroup()
-                                                                .addGap(87, 87, 87)
-                                                                .addComponent(startGameButton, javax.swing.GroupLayout.PREFERRED_SIZE, 263, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                                        .addGroup(backgroundPanelLayout.createSequentialGroup()
-                                                                .addGap(165, 165, 165)
-                                                                .addComponent(exitButton, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE)))))
-                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                                                .addGap(149, 149, 149)
+                                                                .addComponent(leaderboardButton))
+                                                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, backgroundPanelLayout.createSequentialGroup()
+                                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                                .addComponent(exitButton, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                                .addGap(41, 41, 41))))
+                                        .addGroup(backgroundPanelLayout.createSequentialGroup()
+                                                .addGap(567, 567, 567)
+                                                .addComponent(startGameButton, javax.swing.GroupLayout.PREFERRED_SIZE, 263, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                .addGap(75, 75, 75))
                         .addGroup(backgroundPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                .addGroup(backgroundPanelLayout.createSequentialGroup()
-                                        .addComponent(jLayeredPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addGap(0, 0, Short.MAX_VALUE)))
+                                .addComponent(jLayeredPane2))
         );
         backgroundPanelLayout.setVerticalGroup(
                 backgroundPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(backgroundPanelLayout.createSequentialGroup()
                                 .addGap(25, 25, 25)
                                 .addComponent(welcomeMessage)
+                                .addGap(26, 26, 26)
                                 .addGroup(backgroundPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addComponent(logo, javax.swing.GroupLayout.PREFERRED_SIZE, 378, javax.swing.GroupLayout.PREFERRED_SIZE)
                                         .addGroup(backgroundPanelLayout.createSequentialGroup()
-                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                                .addComponent(startGameButton, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addGroup(backgroundPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                                        .addGroup(backgroundPanelLayout.createSequentialGroup()
+                                                                .addGap(190, 190, 190)
+                                                                .addComponent(leaderboardButton))
+                                                        .addGroup(backgroundPanelLayout.createSequentialGroup()
+                                                                .addGap(141, 141, 141)
+                                                                .addComponent(startGameButton)))
                                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                                .addComponent(leaderboardButton)
-                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                                .addComponent(exitButton)
-                                                .addGap(161, 161, 161))
-                                        .addGroup(backgroundPanelLayout.createSequentialGroup()
-                                                .addGap(26, 26, 26)
-                                                .addComponent(logo, javax.swing.GroupLayout.PREFERRED_SIZE, 378, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                                .addContainerGap(42, Short.MAX_VALUE))))
+                                                .addComponent(exitButton)))
+                                .addGap(42, 42, 42))
                         .addGroup(backgroundPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                 .addComponent(jLayeredPane2))
         );
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addComponent(backgroundPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 905, javax.swing.GroupLayout.PREFERRED_SIZE)
-        );
-        layout.setVerticalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addComponent(backgroundPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
+        getContentPane().add(backgroundPanel);
+        backgroundPanel.setBounds(0, 0, 905, 524);
 
         pack();
-    }
-
+    }// </editor-fold>
     private void exitButtonActionPerformed(java.awt.event.ActionEvent evt) {
         wordyImpl.exit(username);
         dispose();
