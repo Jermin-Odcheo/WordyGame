10f813591b18f91e279d7ed445e209a0ff5a8e23
rabino <2225217@slu.edu.ph>
2023-05-26 01:31:14 +0800

    display LongestWords from sql

--- src/Client_Java/LongestWordsUI.java
+++ src/Client_Java/LongestWordsUI.java
@@ -2,12 +2,17 @@
 package Client_Java;
 
 
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.sql.*;
+
 public class LongestWordsUI extends javax.swing.JFrame {
 
     static String username;
     public LongestWordsUI(String username) {
         this.username= username;
         initComponents();
+        displayWordList();
     }
 
     private void initComponents() {
@@ -17,22 +22,17 @@ public class LongestWordsUI extends javax.swing.JFrame {
         jTable1 = new javax.swing.JTable();
         jLabel1 = new javax.swing.JLabel();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         setBackground(new java.awt.Color(255, 51, 255));
 
         jPanel1.setBackground(new java.awt.Color(204, 153, 255));
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
                 new Object [][] {
-                        {null, null, null},
-                        {null, null, null},
-                        {null, null, null},
-                        {null, null, null},
-                        {null, null, null},
-                        {null, null, null}
+
                 },
                 new String [] {
-                        "No.", "PLAYER", "WORD"
+                        "User ID", "WORD"
                 }
         ));
         jScrollPane1.setViewportView(jTable1);
@@ -75,6 +75,27 @@ public class LongestWordsUI extends javax.swing.JFrame {
         );
 
         pack();
+    }
+    private void displayWordList() {
+        String url = "jdbc:mysql://localhost:3306/wordy_accounts";
+        String username = "root";
+        String password = "";
+
+        try (Connection conn = DriverManager.getConnection(url, username, password);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT user_id, word FROM wordlist")) {
+
+            DefaultTableModel model = (DefaultTableModel) jTable1.getModel();
+            model.setRowCount(0);
+
+            while (rs.next()) {
+                int userId = rs.getInt("user_id");
+                String word = rs.getString("word");
+                model.addRow(new Object[]{userId, word});
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void startLongestWordsUI(String username) {
